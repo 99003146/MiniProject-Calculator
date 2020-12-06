e3b3280714a78d36f4759ad39d3dfcbdfd4a8983
99003146 <alen.george@ltts.com>
2020-12-06 19:27:22 +0530

    Coversion Functions

--- Implementation/inc/calculator.h
+++ Implementation/inc/calculator.h
@@ -8,4 +8,12 @@ double divide(double, double);
 double square(double);
 double sqarert(double);
 
+double ctof(double);
+double ftoc(double);
+double kmtom(double);
+double mtokm(double);
+double kgtog(double);
+double gtokg(double);
+
+
 #endif
--- Implementation/src/calculator.c
+++ Implementation/src/calculator.c
@@ -35,3 +35,75 @@ double sqarert(double number)
 {
     return sqrt(number);
 }
+
+
+//conversion
+
+double ctof(double celsius)
+{
+    double fahrenheit;
+    fahrenheit = (celsius * 9 / 5) + 32;
+    return fahrenheit;
+}
+
+double ftoc(double fahrenheit)
+{
+    double celsius;
+    celsius = ((fahrenheit-32)*5)/9;
+    return celsius;
+}
+
+double kmtom(double km)
+{   if(km>=0)
+    {
+    double meter;
+    meter=km*1000;
+    return meter;
+    }
+    else
+    {
+        return 0;
+    }
+    
+}
+
+double mtokm(double meter)
+{
+    int km;
+    if(km>=0)
+    {
+    km=meter/1000;
+    return km;
+    }
+    else
+    {
+        return 0;
+    }
+    
+}
+
+double kgtog(double kg)
+{   if(kg>=0)
+    {
+    double gram=kg*1000;
+    return gram;
+    }
+    else
+    {
+        return 0;
+    }
+    
+}
+
+double gtokg(double gram)
+{   if(gram>=0)
+    {
+    double kg=gram/1000;
+    return kg;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
