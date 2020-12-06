0db6eae21d702dbc52f32914f308fd7ae79b134c
Sagar-Mohan-Jadhav <69190284+Sagar-Mohan-Jadhav@users.noreply.github.com>
2020-12-06 19:05:04 +0530

    Arithametic Functions

---
+++ Implementation/inc/calculator.h
@@ -0,0 +1,11 @@
+#ifndef __CALCULATOR_H__
+#define __CALCULATOR_H__
+
+int add(int, int);
+int substract(int, int);
+double multiply(double, double);
+double divide(double, double);
+double square(double);
+double sqarert(double);
+
+#endif
--- Implementation/src/calculator.c
+++ Implementation/src/calculator.c
@@ -1,2 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 
+int add (int number1, int number2)
+{
+    return number1+number2;
 
+}
+int substract(int number1, int number2)
+{
+    return number1-number2;
+}
+
+double multiply(double number1, double number2)
+{
+    return number1*number2;
+}
+double divide(double number1, double number2)
+{   if(number2!=0)
+    {
+    return number1/number2;
+    }
+    else
+    {
+        return 0;
+    }
+    
+}
+double square(double number)
+{
+    return number*number;
+}
+double sqarert(double number)
+{
+    return sqrt(number);
+}
